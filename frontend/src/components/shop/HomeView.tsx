4c7e65f9d790cf1de21be390938a2b4b21816820
Htoo Kyaw <htookyaw.dev@gmail.com>
2026-03-22 11:29:30 +0700

    fix: remove unused

--- frontend/src/components/shop/HomeView.tsx
+++ frontend/src/components/shop/HomeView.tsx
@@ -1,5 +1,4 @@
 import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
 import { Carousel } from "./Carousel";
 import { useGetBrandsQuery } from "../../lib/api";
 
